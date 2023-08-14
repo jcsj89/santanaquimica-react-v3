5c3524ae99306dbe6843015187305e90fde373c2
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2023-08-14 16:35:00 +0100

    14.08.23

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <>
       <div className="mt-20 mx-auto items-center justify-center flex gap-2 text-2xl font-robotomono">
-        <p>© Santana Quimica. All rights reserved. Site by ZeSantana</p>
+        <p>© Santana Quimica. All rights reserved. Site by ZeSantana !</p>
       </div>
     </>
   );
